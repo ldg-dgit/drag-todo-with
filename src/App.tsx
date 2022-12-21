9f8ff30cbb5e056f0a8b95aeae59110005b1e834
Bobby Lee <ldg@dgit.co>
2022-12-21 13:03:22 +0900

    practice Se t Selectors & finish Hour/Minute translator

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { hourSelector, minuteState } from "./atoms";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector);
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
   };
   return (
     <div>
       <input value={minutes} onChange={onMinutesChange} type='number' placeholder='Minutes' />
-      <input value={hours} type='number' placeholder='Hours' />
+      <input onChange={onHoursChange} value={hours} type='number' placeholder='Hours' />
     </div>
   );
 }
