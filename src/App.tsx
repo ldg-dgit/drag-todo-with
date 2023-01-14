80dfd43026ec7b01290cd461d87bfe4d27a2cb11
Bobby Lee <ldg@dgit.co>
2023-01-14 11:11:16 +0900

    cross Board Movement

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ function App() {
   const [todos, setTodos] = useRecoilState(todoState);
   const onDragEnd = (info: DropResult) => {
     const { destination, draggableId, source } = info;
+    if (!destination) return;
     if (destination?.droppableId === source.droppableId) {
       setTodos((allBoards) => {
         const boardCopy = [...allBoards[source.droppableId]];
@@ -33,6 +34,19 @@ function App() {
         return {
           ...allBoards,
           [source.droppableId]: boardCopy,
+        };
+      });
+    }
+    if (destination.droppableId !== source.droppableId) {
+      setTodos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination?.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
         };
       });
     }
